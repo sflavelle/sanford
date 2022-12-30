14847d673d8d9dcb7c888f4dc02f1f20178c9ebb
Simon Flavelle <me@neurario.com>
2022-12-30 17:20:58 +1100

    Masto commands and work on PluralKit integrations

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 # Import essential libraries
 import discord
 from discord import app_commands
+from discord.ext import commands
+from pluralkit import Client as PK
+import asyncio
 from typing import Optional
 from datetime import datetime
 import yaml
@@ -9,6 +12,9 @@ import sqlite3
 # Import custom libraries
 from helpers.quoting import format_quote,fetch_random_quote
 from mastoposter import post_new_quote
+
+# init vars?
+cfg = None
 
 # load config
 with open('config.yaml', 'r') as file:
@@ -21,6 +27,7 @@ db = "db/quotes.sqlite"
 intents = discord.Intents.default()
 intents.message_content = True
 
+Owner = discord.Object(id=49288117307310080)
 TGC = discord.Object(id=124680630075260928)
 
 # setup command framework
@@ -40,10 +47,14 @@ class Sanford(discord.Client):
     
     async def setup_hook(self):
         # self.tree.copy_global_to(guild=TGC) # Sync all cmds to General Chat only (for now)
+        await self.tree.sync(guild=TGC)
         await self.tree.sync()
 
 # init client
 sanford = Sanford(intents=intents)
+
+# init Pluralkit API
+pluralkit = PK(cfg['sanford']['pluralkit_token'], async_mode=False)
 
 @sanford.event
 async def on_ready():
@@ -53,9 +64,24 @@ async def on_ready():
 
 @sanford.tree.command()
 @app_commands.guilds(TGC)
-async def poke(interaction: discord.Interaction):
-    """Maybe see if Sanford is around?"""
-    await interaction.response.send_message('`Mhhh. Come back later...`')
+@app_commands.rename(exclude_in_mastoposter='dont_send_quotes')
+@app_commands.describe(exclude_in_mastoposter="Exclude your quotes from being posted to @tgcooc?")
+async def mastodon(interaction: discord.Interaction, exclude_in_mastoposter: bool):
+    global cfg
+    """Configure settings relating to you in the @tgcooc Mastodon account."""
+    try:
+        if exclude_in_mastoposter is not None:
+            if exclude_in_mastoposter == True:
+                cfg['mastodon']['exclude_users'].append(interaction.user.id)
+            else: cfg['mastodon']['exclude_users'].remove(interaction.user.id)
+            with open('config.yaml', 'w') as file:
+                yaml.dump(cfg, file)
+            with open('config.yaml', 'r') as file:
+                cfg = yaml.safe_load(file)
+            await interaction.response.send_message("Your settings were updated.",ephemeral=True)
+    except io.UnsupportedOperation as error:
+        await interaction.response.send_message("There was an issue reading the file.",ephemeral=True)
+
 
 @sanford.tree.command()
 async def quote(interaction: discord.Interaction):
@@ -78,7 +104,7 @@ async def quote_save(interaction: discord.Interaction, message: discord.Message)
         if check_dupe.fetchone() is not None:
             raise LookupError('This quote is already in the database.')
 
-        sql_values = (
+        sql_values = list((
             message.content, 
             message.author.id,
             message.author.name,
@@ -88,7 +114,14 @@ async def quote_save(interaction: discord.Interaction, message: discord.Message)
             int(datetime.timestamp(message.created_at)),
             datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f %z"),
             datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f %z")
-            )
+            ))
+
+        # # PluralKit check
+        # if message.webhook_id:
+        #     pkmsg = pluralkit.get_message(message.id)
+        #     print(pkmsg)
+        #     if pkmsg is not None:
+        #         sql_values[1] = int(pkmsg.sender)
         
         cur.execute("INSERT INTO quotes (content, authorID, authorName, addedBy, guild, msgID, timestamp, createdAt, updatedAt) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);", sql_values)
         con.commit()
@@ -101,7 +134,7 @@ async def quote_save(interaction: discord.Interaction, message: discord.Message)
             allowed_mentions=discord.AllowedMentions.none()
             )
         
-        if interaction.guild_id == 124680630075260928:
+        if interaction.guild_id == 124680630075260928 and message.author.id not in cfg['mastodon']['exclude_users']:
             post_new_quote(message.content, message.author.id, int(message.created_at.timestamp()))
         
         con.close()
